dd0d8780d6727d419b631d0e175022dfac8b2858
Adrian Sepulveda <adrian.dummy67@gmail.com>
2024-11-27 01:10:45 +0800

    Adrian

--- src/Pages/Landingpage.js
+++ src/Pages/Landingpage.js
@@ -15,20 +15,21 @@ function LandingPage() {
 
     return (
         <div className="landing-container">
-            <nav className="navbar">
-                <div className="navbar-logo">
-                    <img src={logo} alt="Excellent Knights PH Logo" className="logo-img" />
-                </div>
-                <div className="navbar-links-left">
-                    <button className="login-btn" onClick={handleLogin}>
-                        Log In
-                    </button>
-                    <button className="register-btn" onClick={handleRegister}>
-                        Register
-                    </button>
-                </div>
-            </nav>
+    <nav className="navbar">
+        <div className="navbar-logo">
+            <img src={logo} alt="Excellent Knights PH Logo" className="logo-img" /> {/* Logo first */}
+            <div className="club-name">Excellent Knights PH</div> {/* Club name after the logo */}
         </div>
+        <div className="navbar-links-left">
+            <button className="login-btn" onClick={handleLogin}>
+                Log In
+            </button>
+            <button className="register-btn" onClick={handleRegister}>
+                Register
+            </button>
+        </div>
+    </nav>
+</div>
     );
 }
 
